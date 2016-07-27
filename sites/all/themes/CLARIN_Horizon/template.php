567554590aa54e054e8d1869a99507b565e5144a
Sander Maijers <S.N.Maijers@gmail.com>
2016-07-27 14:16:40 +0200

    Add ‘CLARIN Horizon’ theme
    Add our Bootstrap for Drupal subtheme.
    
    Based on Bootstrap 3.3.6 and LESS starterkit 7.x-3.5.

---
+++ sites/all/themes/CLARIN_Horizon/template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * The primary PHP file for this theme.
+ */
+
+/**
+ * Overrides theme_menu_link().
+ */
+function CLARIN_Horizon_menu_link__menu_block($variables) {
+	return theme_menu_link($variables);
+}
+?>
