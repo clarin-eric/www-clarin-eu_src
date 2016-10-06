0df39dd2cfa64cece9df1216021b24201cf50032
Sander Maijers <S.N.Maijers@gmail.com>
2016-10-06 18:13:17 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -6,17 +6,17 @@ set -e
 npm set progress='false'
 npm --silent install --depth '0' --global 'less' 'less-plugin-clean-css'
 curl --fail --location --show-error --silent --tlsv1 \
-    'https://github.com/drupalprojects/bootstrap/archive/7.x-3.5.tar.gz' | \
+    'https://github.com/drupalprojects/bootstrap/archive/7.x-3.6.tar.gz' | \
         tar -x -z -p -f -
 ## Use the Less-based starter kit.
 cp -apr -- 'bootstrap-7.x-3.5/starterkits/less' ~/'CLARIN_Horizon/'
 ## Edit subtheme properties.
-rm -- 'less.subtheme'
-# TODO
+cd -- ~/'CLARIN_Horizon/'
+rm -- 'less.starterkit'
 ## Customize graphics.
 (cd 'sites/all/themes/CLARIN_Horizon/'
 mv -f -- 'CLARIN_Horizon.info' 'favicon.ico' 'logo.png' ~/'CLARIN_Horizon/')
-cd -- ~/'CLARIN_Horizon/'
+rm -rf -- 'bootstrap/'
 git clone 'git@github.com:twbs/bootstrap.git'
 (cd -- ~/'bootstrap'
 git fetch --tags
