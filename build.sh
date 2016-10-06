90af4a06ff6f61fd899bce602e9acecd08bf98ef
Sander Maijers <S.N.Maijers@gmail.com>
2016-10-06 17:32:26 +0200

    Build and release via Travis CI and GitHub

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+# TODO
+# ~/.npmrc
+npm set progress='false'
+npm --silent install --depth '0' --global 'less' 'less-plugin-clean-css'
+curl --fail --location --show-error --silent --tlsv1 \
+    'https://github.com/drupalprojects/bootstrap/archive/7.x-3.6.tar.gz' | \
+        tar -x -z -p -f -
+## Use the Less-based starter kit.
+cp -apr -- 'bootstrap-7.x-3.6/starterkits/less' ~/'CLARIN_Horizon/'
+## Customize graphics.
+(cd 'sites/all/themes/CLARIN_Horizon/'
+mv -f -- 'CLARIN_Horizon.info' 'favicon.ico' 'logo.png' ~/'CLARIN_Horizon/')
+cd -- ~/'CLARIN_Horizon/'
+rm -fr -- 'less.starterkit' 'bootstrap/'
+# TODO: Do not clone but download archive. This has not worked well in the past.
+# Reattempt.
+git clone 'https://github.com/twbs/bootstrap.git'
+(cd -- 'bootstrap/'
+git fetch --tags
+git checkout 'v3.3.6'
+rm -fr -- '.git/')
+## Apply CLARIN base style.
+curl --fail --location --show-error --silent --tlsv1 \
+    'https://github.com/clarin-eric/base_style/releases/download/0.1.1/variables.less' > 'less/variable-overrides.less'
+lessc 'less/style.less' --clean-css='--s0' > 'css/style.css'
+tar -c -p -z -f ~/CLARIN_Horizon.tgz .
