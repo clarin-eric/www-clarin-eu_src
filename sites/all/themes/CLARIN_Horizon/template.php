48f07c75471e695c436efaab8da6d4469725180f
Twan Goosen <twan.goosen@mpi.nl>
2017-01-11 13:57:55 +0100

    Adding font css links to the header via CLARIN Horizon template

--- sites/all/themes/CLARIN_Horizon/template.php
+++ sites/all/themes/CLARIN_Horizon/template.php
@@ -5,6 +5,34 @@
  */
 
 /**
+ * Overrides theme_preprocess_html().
+ */
+function CLARIN_Horizon_preprocess_html(&$vars) {
+  $fontCssLinkSourceSansPro = array(
+    '#type' => 'html_tag',
+    '#tag' => 'link',
+    '#attributes' => array(
+      'href' =>  'https://fonts.googleapis.com/css?family=Source+Sans+Pro',
+      'rel' => 'stylesheet',
+      'type' => 'text/css'
+    )
+  );
+  
+  $fontCssLinkRobotoSlab = array(
+    '#type' => 'html_tag',
+    '#tag' => 'link',
+    '#attributes' => array(
+      'href' =>  'https://fonts.googleapis.com/css?family=Roboto+Slab',
+      'rel' => 'stylesheet',
+      'type' => 'text/css'
+    )
+  );  
+  // Add header meta tag for IE to head
+  drupal_add_html_head($fontCssLinkSourceSansPro, 'fontCssLinkSourceSansPro');
+  drupal_add_html_head($fontCssLinkRobotoSlab, 'fontCssLinkRobotoSlab');
+}
+
+/**
  * Overrides theme_menu_link().
  */
 function CLARIN_Horizon_menu_link__menu_block($variables) {
